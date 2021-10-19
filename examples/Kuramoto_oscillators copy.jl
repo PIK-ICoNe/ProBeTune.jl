5d59b2bcb57241f4163e25c8f2cfdf76e77af80c
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2021-10-19 17:35:21 +0200

    Further tuning for the system with s=1.2

--- examples/Kuramoto_oscillators copy.jl
+++ examples/Kuramoto_oscillators copy.jl
@@ -17,7 +17,7 @@ Random.seed!(1)
 ##
 include("PlotUtils.jl")
 ##
-path = "../plots/kuramoto/new/"
+path = "../plots/kuramoto/"
 ##
 global t_steps = 0.:0.1:10.
 global tspan = (0., 10.)
@@ -48,7 +48,7 @@ function tune_run(omega, scale; save_plots = false)
     ##
     scen = 1:3
     ##
-    plot_callback(kur, p_dist, d, scenario_nums = scen, title = "Scenarios $scen, s = $scale", xlims = (kur.t_span[2]/2, kur.t_span[2]))
+    plot_callback(kur, p_dist, d, scenario_nums = scen, title = "Scenarios $scen, s = $scale", offset = 0.5, xlims = (kur.t_span[2]/2, kur.t_span[2]))
     if save_plots
         savefig(path*string(round(scale, digits = 4))*"_initial_"*string(round(d, digits = 4))*".png")
     end
@@ -70,7 +70,7 @@ function tune_run(omega, scale; save_plots = false)
     
     p_t2 = res.minimizer
     ##
-    plot_callback(kur, p_t2, res.minimum, scenario_nums = scen, title = "Scenarios $scen, s = $scale", xlims = (kur.t_span[2]/2, kur.t_span[2]))
+    plot_callback(kur, p_t2, res.minimum, scenario_nums = scen, title = "Scenarios $scen, s = $scale", offset = 0.5, xlims = (kur.t_span[2]/2, kur.t_span[2]))
 
     if save_plots
         savefig(path*string(round(scale, digits = 4))*"_tuned_"*string(round(res.minimum, digits = 4))*".png")
@@ -80,7 +80,7 @@ function tune_run(omega, scale; save_plots = false)
 
     println("After a second tuning using BFGS the distance to specified behaviour is lower than $d3\n")
     ##
-    plot_callback(kur, p_dist_3, d3, scenario_nums = scen, title = "Scenarios $scen, s = $scale", xlims = (kur.t_span[2]/2, kur.t_span[2]))
+    plot_callback(kur, p_dist_3, d3, scenario_nums = scen, title = "Scenarios $scen, s = $scale", offset = 0.5, xlims = (kur.t_span[2]/2, kur.t_span[2]))
     if save_plots
         savefig(path*string(round(scale, digits = 4))*"_tuned2_"*string(round(d3, digits = 4))*".png")
     end
@@ -96,7 +96,7 @@ function tune_run(omega, scale; save_plots = false)
                     optimizer_options=(
                         :maxiters => 1000,))
     
-    plot_callback(kur, p_dist_5, d5, scenario_nums = scen, title = "Scenarios $scen, s = $scale", xlims = (kur.t_span[2]/2, kur.t_span[2]))
+    plot_callback(kur, p_dist_5, d5, scenario_nums = scen, title = "Scenarios $scen, s = $scale", offset = 0.5, xlims = (kur.t_span[2]/2, kur.t_span[2]))
     if save_plots
         savefig(path*string(round(scale, digits = 4))*"_final_rs_"*string(round(d5, digits = 4))*".png")
     end
@@ -117,9 +117,68 @@ res = [tune_run(omega, s, save_plots = true) for s in scales]
 q = [r[1]/r[2] for r in res]
 
 plot(scales, q)
-savefig("../plots/kur/improvement(scale).png")
-scales2 = 6.:19.
-res2 = [tune_run(omega, s, save_plots = true) for s in scales2]
+savefig(path*"improvement(scale).png")
+#scales2 = 6.:19.
+#res2 = [tune_run(omega, s, save_plots = true) for s in scales2]
 
 plot(scales, [r[2] for r in res], xlabel = "s", ylabel = "d", label = "Final distance to specification")
-savefig("../plots/kur/final_d(omega).png")
+savefig(path*"final_d(omega).png")
+
+## Continue optimization for s=1.2
+kur_new = PBTExample.create_kuramoto_example(omega*1.2, N_osc, size_p_spec, K_av, t_steps, N_samples)
+p_initial_new = res[3][3] # for scale = 1.2
+
+d, p_dist, = behavioural_distance(kur_new, p_initial_new; abstol=1e-4, reltol=1e-4,
+                    optimizer = DiffEqFlux.BFGS(),
+                    optimizer_options=(
+                        :maxiters => 10, 
+                        :cb => PBTLibrary.basic_pbt_callback))
+##
+scen = 1:3
+plot_callback(kur_new, p_dist, d, scenario_nums = scen, title = "Scenarios $scen, s = 1.2", offset = 0.5, xlims = (kur_new.t_span[2]/2, kur_new.t_span[2]))
+
+
+##
+d, p_dist, = behavioural_distance(kur_new, p_initial_new; abstol=1e-4, reltol=1e-4,
+                    optimizer = DiffEqFlux.BFGS(),
+                    optimizer_options=(
+                        :maxiters => 10, 
+                        :cb => PBTLibrary.basic_pbt_callback))
+
+plot_callback(kur_new, p_dist, d, scenario_nums = scen, title = "Scenarios $scen, s = 1.2", offset = 0.5, xlims = (kur_new.t_span[2]/2, kur_new.t_span[2]))
+##
+res = pbt_tuning(kur_new, p_dist)
+
+plot_callback(kur_new, res.minimizer, res.minimum, scenario_nums = scen, title = "Scenarios $scen, s = 1.2", offset = 0.5, xlims = (kur_new.t_span[2]/2, kur_new.t_span[2]))
+
+savefig(path*"1.2_tuned_3_"*string(round(res.minimum, digits = 4))*".png")
+
+resample!(kur_new)
+
+d_rs, p_dist_rs = behavioural_distance(kur_new, res.minimizer; abstol=1e-4, reltol=1e-4,
+optimizer = DiffEqFlux.BFGS(),
+optimizer_options=(
+    :maxiters => 10, 
+    :cb => PBTLibrary.basic_pbt_callback))
+
+plot_callback(kur_new, p_dist_rs, d_rs, scenario_nums = scen, title = "Scenarios $scen, s = 1.2", offset = 0.5, xlims = (kur_new.t_span[2]/2, kur_new.t_span[2]))
+
+savefig(path*"1.2_rs_further_"*string(round(d_rs, digits = 4))*".png")
+##
+res2 = pbt_tuning(kur_new, p_dist_rs)
+
+plot_callback(kur_new, res2.minimizer, res2.minimum, scenario_nums = scen, title = "Scenarios $scen, s = 1.2", offset = 0.5, xlims = (kur_new.t_span[2]/2, kur_new.t_span[2]))
+
+savefig(path*"1.2_rtuned4_"*string(round(res2.minimum, digits = 4))*".png")
+##
+resample!(kur_new)
+
+d_rs, p_dist_rs = behavioural_distance(kur_new, res.minimizer; abstol=1e-4, reltol=1e-4,
+optimizer = DiffEqFlux.BFGS(),
+optimizer_options=(
+    :maxiters => 10, 
+    :cb => PBTLibrary.basic_pbt_callback))
+
+plot_callback(kur_new, p_dist_rs, d_rs, scenario_nums = scen, title = "Scenarios $scen, s = 1.2", offset = 0.5, xlims = (kur_new.t_span[2]/2, kur_new.t_span[2]))
+
+savefig(path*"1.2_rs_final_"*string(round(d_rs, digits = 4))*".png")
